fc3a3689fa45310117ac0438a7007a887b525dd7
Kenshi Shiode <atemu1212@yahoo.co.jp>
2022-01-19 23:01:02 +0900

    update

--- pump/pump.ino
+++ pump/pump.ino
@@ -1,15 +1,17 @@
 #define pumpPin 13
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(pumpPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(pumpPin, HIGH);
-  delay(2000);
+  if(Serial.available() > 0) {
+    Serial.read();
+    digitalWrite(pumpPin, HIGH);
+    delay(1000);
+    return;
+  } 
+
   digitalWrite(pumpPin, LOW);
-  delay(2000);
 }
